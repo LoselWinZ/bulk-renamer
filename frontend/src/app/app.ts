384eaed596687ce146fbd8f43bef61443e4567ba
Niclas Antl <n.antl@gmx.de>
2025-11-15 16:51:52 +0100

    initial commit

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,13 @@
+import {Component, signal} from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    ButtonModule
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+}
